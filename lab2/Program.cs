7abbf584131d21f6c0f4b39badf4e7ea965b3808
SaRu-N <saruniroula73@gmail.com>
2022-08-20 13:35:06 +0545

    Lab 2: class and object

---
+++ lab2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace NccLab
+{
+    class program{
+       
+        public static void Main(String[] args){
+             string choice="N";
+        do{
+
+        classandobjectexample(); 
+        System.Console.WriteLine("Do you want to continue?(Y/N):");
+        choice= Console.ReadLine();
+        }while(choice.ToUpper()=="Y");
+        return;
+        }
+        static void classandobjectexample(){
+            // creating object of the class
+            classandobjects student =new classandobjects();
+            student.Name="Saraswati Niroula";
+            student.Roll=20926;
+            student.Class="Bsc CSIT 6th Sem";
+            System.Console.WriteLine("Students Information");
+            System.Console.WriteLine($"Name={student.Name} Roll Number={student.Roll} Class={student.Class}");
+}
+    }
+
+}
---
+++ lab2/classandobjects.cs
@@ -0,0 +1,10 @@
+using System;
+namespace NccLab
+{
+    // Defining a class
+    public class classandobjects{
+       public string Name {get;set;}
+       public int Roll {get;set;}
+       public string Class{get;set;}
+    }
+}
